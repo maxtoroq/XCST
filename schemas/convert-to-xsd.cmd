7ae5adca6763560045910517377e47e286cd7346
Max Toro <maxtoroq@gmail.com>
2015-11-05 21:18:32 -0300

    app schema

--- schemas/convert-to-xsd.cmd
+++ schemas/convert-to-xsd.cmd
@@ -2,3 +2,4 @@
 cd %~dp0
 
 java -jar \lib\trang\20091111\trang.jar -o any-process-contents=lax -o indent=3 xcst.rng xcst.xsd
+java -jar \lib\trang\20091111\trang.jar -o any-process-contents=lax -o indent=3 xcst-app.rng xcst-app.xsd
