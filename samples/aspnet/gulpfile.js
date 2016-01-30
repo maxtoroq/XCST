01e45437192f20e07bd68aa9eddfa1225885844b
Max Toro <maxtoroq@gmail.com>
2016-01-30 12:38:51 -0300

    samples changes

--- samples/aspnet/gulpfile.js
+++ samples/aspnet/gulpfile.js
@@ -2,45 +2,23 @@
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 
-gulp.task('all-js-css', ['copy-css', 'less-and-minify', 'copy-js']);
+gulp.task('all-js-css', ['copy-precompiled', 'compile-css']);
 
-gulp.task('copy-css', function () {
+gulp.task('copy-precompiled', function () {
 
    var source = 'bower_components/';
    var dest = 'css/';
-
-   // copy pre-compiled files
 
    gulp.src('bootstrap/dist/css/*.*', { cwd: source })
       .pipe(gulp.dest(dest));
 
    gulp.src('bootstrap/dist/fonts/*.*', { cwd: source })
       .pipe(gulp.dest('fonts'));
-});
 
-gulp.task('less-and-minify', function () {
-
-   var dest = 'css/';
-
-   // less
-
-   gulp.src('less/*.less')
-      .pipe(less())
-      .pipe(gulp.dest(dest))
-      .pipe(minifyCSS())
-      .pipe(rename({ extname: '.min.css' }))
-      .pipe(gulp.dest(dest));
-});
-
-gulp.task('copy-js', function () {
-
-   var source = 'bower_components/';
-   var dest = 'js/';
-
-   // copy pre-compiled files
+   dest = 'js/';
 
    gulp.src([
       'jquery/dist/jquery?(.min).{js,map}'
@@ -48,5 +26,17 @@ gulp.task('copy-js', function () {
       , 'jquery-validation-unobtrusive/jquery.validate.unobtrusive?(.min).{js,map}'
       , 'bootstrap/dist/js/bootstrap?(.min).{js,map}'
    ], { cwd: source })
+      .pipe(gulp.dest(dest));
+});
+
+gulp.task('compile-css', function () {
+
+   var dest = 'css/';
+
+   gulp.src('_scss/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest(dest))
+      .pipe(minifyCSS())
+      .pipe(rename({ extname: '.min.css' }))
       .pipe(gulp.dest(dest));
 });
